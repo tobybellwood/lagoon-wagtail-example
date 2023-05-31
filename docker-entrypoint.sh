3c1bc33279dfdb61c3b82dd4c8bef2a82c3995a6
Chris <chris_2190@hotmail.com>
2023-05-31 11:28:35 +1000

    Included entrypoint

---
+++ docker-entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+
+/code/mysite/manage.py migrate
+
+if [ ! -z $DJANGO_SUPERUSER_USERNAME ] && [ ! -z $DJANGO_SUPERUSER_PASSWORD ] && [ ! -z $DJANGO_SUPERUSER_EMAIL ]; then
+	/code/mysite/manage.py createsuperuser --no-input
+fi
+
+uwsgi --http :8800 --module mysite.wsgi
+
+exec "$@"
--- mysite/mysite/settings/base.py
+++ mysite/mysite/settings/base.py
@@ -84,11 +84,13 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    }
+    'default': dj_database_url.config(
+        conn_max_age=600,
+        conn_health_checks=True,
+    ),
 }
 
 
